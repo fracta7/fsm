ca8ce8fab2a6ebbf0648d33b0847bc532ca57c8b
Javokhir Matnazarov <fracta7@gmail.com>
2024-10-30 23:53:07 +0500

    moved the function call of ncurses to the game loop

--- frontend.c
+++ frontend.c
@@ -137,19 +137,6 @@ void draw_pause() {
 void draw_state(GameInfo_t state, UserAction_t event) {
   static bool paused = false;
   // Initialize ncurses
-  initscr();  // Start ncurses mode
-  start_color();
-  cbreak();              // Disable line buffering, pass on everything
-  noecho();              // Do not print user input
-  keypad(stdscr, TRUE);  // Enable special keys (e.g., arrow keys)
-  nodelay(stdscr, TRUE);
-  curs_set(FALSE);
-
-  init_pair(1, COLOR_WHITE, COLOR_WHITE);  // Text white, background blue
-  init_pair(2, COLOR_BLUE, COLOR_BLUE);    // Text white, background blue
-  init_pair(3, COLOR_GREEN, COLOR_BLACK);
-  init_pair(4, COLOR_WHITE, COLOR_RED);
-  init_pair(5, COLOR_WHITE, COLOR_BLACK);
 
   draw_borders();
   draw_next(state);
--- fsm.c
+++ fsm.c
@@ -8,11 +8,26 @@ void game_loop(InputHandlerCallback callback) {
   UserAction_t event;
   int running = 1;
   init_state(&state);
+
+  initscr();  // Start ncurses mode
+  start_color();
+  cbreak();              // Disable line buffering, pass on everything
+  noecho();              // Do not print user input
+  keypad(stdscr, TRUE);  // Enable special keys (e.g., arrow keys)
+  nodelay(stdscr, TRUE);
+  curs_set(FALSE);
+
+  init_pair(1, COLOR_WHITE, COLOR_WHITE);  // Text white, background blue
+  init_pair(2, COLOR_BLUE, COLOR_BLUE);    // Text white, background blue
+  init_pair(3, COLOR_GREEN, COLOR_BLACK);
+  init_pair(4, COLOR_WHITE, COLOR_RED);
+  init_pair(5, COLOR_WHITE, COLOR_BLACK);
   while (running) {
     bool key_pressed = get_input(&event);
     callback(key_pressed, &running, event, &state);
     draw_state(state, event);
   }
+
   endwin();
   free_state(&state);
 }
